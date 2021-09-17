2e42d46359868988f6ccbc610e9221e0348dee2f
Mahendra Gohil <mgohil984@rku.ac.in>
2021-09-17 08:27:16 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+
+  Text,
+  View,
+} from 'react-native';
+
+
+import Video  from "react-native-video";
+
+const App = () => {
+  
+  return (
+    <SafeAreaView style={{flex:1}}>
+
+      <Text>hfgh</Text>
+   
+    </SafeAreaView>
+  );
+};
+
+export default App;
